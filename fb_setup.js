077dce86363e11bbe2ae58899d458c4016a12abc
Aaron Shaji <23353as@hvhs.school.nz>
2026-04-24 10:00:51 +1200

    Started working on mini skills

--- fb_setup.js
+++ fb_setup.js
@@ -8,13 +8,14 @@
 // Return: n/a
 /**************************************************************/
   const firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    databaseURL: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId:,
-    appId:
+    apiKey: "AIzaSyDoc6oIXoHAGvwK-8MUV-Q4tbx_wvK6UdA",
+    authDomain: "aaron-shaji-12comp.firebaseapp.com",
+    databaseURL: "https://aaron-shaji-12comp-default-rtdb.firebaseio.com",
+    projectId: "aaron-shaji-12comp",
+    storageBucket: "aaron-shaji-12comp.firebasestorage.app",
+    messagingSenderId: "461472100432",
+    appId: "1:461472100432:web:a8646dd71cda8238ee6155",
+    measurementId: "G-9G30RS1KYT"
   };
 
   // Initialize Firebase
@@ -22,5 +23,3 @@
 
   // This log prints the firebase object to the console to show that it is working.
   // As soon as you have the script working, delete this log.
-  console.log("Firebase initialize finished:");
-  console.log(firebase);
--- script.js
+++ script.js
@@ -17,11 +17,35 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
-function helloWorld(){
-  console.log("Running helloWorld()")
-  firebase.database().ref('/').set(
+function bookOne(){
+  console.log("Running bookOne()")
+  firebase.database().ref('Books/Book 1').set(
     {
-      message: 'Hello World!'
+      Title: Random Book,
+      Author: Me,
+      Genre: Non-Fiction,
+    }
+  )
+}
+
+function bookeTwo(){
+  console.log("Running bookTwo()")
+  firebase.database().ref('Books/Book 2').set(
+    {
+      Title: Better Book,
+      Author: Me,
+      Genre: Fiction,
+    }
+  )
+}
+
+function bookThree(){
+  console.log("Running bookThree()")
+  firebase.database().ref('Books/Book 3').set(
+    {
+      Title: Even Better Book,
+      Author: Me,
+      Genre: Dystopian,
     }
   )
 }
