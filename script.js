7189e48270d9b4f2d0c7fb2087a5d965ef4b7e3b
Aaron Shaji <23353as@hvhs.school.nz>
2026-04-28 12:28:18 +1200

    Basic read

--- script.js
+++ script.js
@@ -17,35 +17,66 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
+let lastBook="null"
+
+
+
+
 function bookOne(){
   console.log("Running bookOne()")
+
+  lastBook = "Books/Book 1"
+
   firebase.database().ref('Books/Book 1').set(
     {
-      Title: Random Book,
-      Author: Me,
-      Genre: Non-Fiction,
+      Title: "Random Book",
+      Author: "Me",
+      Genre: "Non-Fiction",
     }
   )
 }
 
-function bookeTwo(){
+function bookTwo(){
   console.log("Running bookTwo()")
+
+  lastBook = "Books/Book 2"
+
   firebase.database().ref('Books/Book 2').set(
     {
-      Title: Better Book,
-      Author: Me,
-      Genre: Fiction,
+      Title: "Better Book",
+      Author: "Myself",
+      Genre: "Fiction",
     }
   )
 }
 
 function bookThree(){
   console.log("Running bookThree()")
+
+  lastBook = "Books/Book 3"
+
   firebase.database().ref('Books/Book 3').set(
     {
-      Title: Even Better Book,
-      Author: Me,
-      Genre: Dystopian,
+      Title: "Even Better Book",
+      Author: "I",
+      Genre: "Dystopian",
     }
   )
+}
+
+function lastBookSelected(){
+  console.log("Reading message");
+  firebase.database().ref(lastBook).once('value', displayLastBookSelected);
+}
+
+function displayLastBookSelected(snapshot) {
+  let book = snapshot.val();
+
+  console.log("Book data:", lastBook);
+
+  HTML_OUTPUT.innerHTML = `
+    Title: ${book.Title}<br>
+    Author: ${book.Author}<br>
+    Genre: ${book.Genre}
+  `;
 }
